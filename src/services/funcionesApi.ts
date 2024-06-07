d93fef28600a4f0c7bd7b5638e0ea3a6633690ca
Guido Pérez Moro <90873217+GuidoPerezMoro@users.noreply.github.com>
2024-06-07 16:59:30 -0300

    Variables de entorno

--- src/services/funcionesApi.ts
+++ src/services/funcionesApi.ts
@@ -1,11 +1,14 @@
-export function getAllInstrumentos(){
-    return fetch(`http://localhost:3000/Instrumentos`)
-            .then(res=>res.json())
-            .then(json=>json)
+// funcionesApi.ts
+const API_URL = import.meta.env.VITE_API_URL;
+
+export function getAllInstrumentos() {
+    return fetch(`${API_URL}/Instrumentos`)
+        .then(res => res.json())
+        .then(json => json);
 }
 
-export function getOneInstrumento(id:number){
-    return fetch(`http://localhost:3000/Instrumentos/${id}`)
-            .then(res=>res.json())
-            .then(json=>json)
+export function getOneInstrumento(id: number) {
+    return fetch(`${API_URL}/Instrumentos/${id}`)
+        .then(res => res.json())
+        .then(json => json);
 }
